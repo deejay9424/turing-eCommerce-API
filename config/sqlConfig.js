3fa9a484c5843062ad1d227abfa15fdd69ba4217
deejay9424 <dhruv.voith@gmail.com>
2019-06-22 20:05:55 +0530

    new db config

--- config/sqlConfig.js
+++ config/sqlConfig.js
@@ -1,22 +1,26 @@
 const mysql = require('mysql');
 const chalk = require('chalk');
 const logger = require('../config/logger').Logger;
+require('dotenv').config();
 const VariableStore = require("../config/VariableStore");
 
 var con = mysql.createConnection({
-    host: "sql3.freesqldatabase.com",
-    user: "sql3295598",
-    password: "2WE6Biqtbb",
-    database: "sql3295598"
+    host: process.env.DatabaseHostname,
+    user: process.env.DatabaseUsername,
+    password: process.env.DatabasePassword,
+    database: process.env.DatabaseName
 });
 
 con.connect(function (err) {
-    if (err){
-        logger.error("Error in SQL Database connection : "+err)
+    if (err) {
+        logger.error("Error in SQL Database connection : " + err)
         console.log(chalk.red("Error Connecting to Database"))
         return new Error(VariableStore.Errors.databaseError)
     }
     console.log(chalk.green("Database Connected!"));
 });
-con.on('error', console.error.bind(chalk.yellow('connection error...')));
+con.on('error', function (err) {
+    logger.error("Error in DB connection : " + err)
+    console.error.bind(chalk.yellow('connection error...'))
+});
 module.exports = con;
